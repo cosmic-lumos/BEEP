5be4825d826edacaf1a7a344556fab620fcb400a
이동율 <dy9623@naver.com>
2024-02-16 17:08:07 +0900

    feat: 메인페이지 초기 설정

---
+++ src/pages/mainPage.jsx
@@ -0,0 +1,14 @@
+import "../styles/mainPage.css";
+
+function MainPage() {
+  return (
+    <div className="container">
+      <div className="innerContainer">
+        <div className="borrowedItemListComponent"></div>
+        <div className="checkStockComponent"></div>
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
