4d87222dfa3734f1c3cb73adc61f5c39ccbe40cd
이동율 <dy9623@naver.com>
2024-02-16 17:53:31 +0900

    feat:header 추가

--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -3,6 +3,7 @@ import "../styles/mainPage.css";
 function MainPage() {
   return (
     <div className="container">
+      <div className="header">BEEP</div>
       <div className="innerContainer">
         <div className="borrowedItemListComponent"></div>
         <div className="checkStockComponent"></div>
