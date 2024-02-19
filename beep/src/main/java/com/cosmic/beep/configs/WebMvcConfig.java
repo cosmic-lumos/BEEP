bea93bf3d14d0fbf9d7c10bfe77a3b5f4b3c67dc
BiteSnail <rkwoal216@gmail.com>
2024-02-19 11:00:23 +0900

    feat: WebMVCConfig 추가

---
+++ beep/src/main/java/com/cosmic/beep/configs/WebMvcConfig.java
@@ -0,0 +1,11 @@
+package com.cosmic.beep.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+}
