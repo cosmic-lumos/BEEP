813a8f9f8a947e95f0718af210f358fc1c07f492
이동율 <dy9623@naver.com>
2024-02-22 12:48:50 +0900

    feat: 로그인 페이지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import MainPage from "./pages/mainPage";
+import LoginPage from "./pages/loginPage";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         {/* 웹 서비스 소개 페이지 */}
         <Route path="/main" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/loginPage.jsx
@@ -0,0 +1,139 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #212529;
+`;
+
+const HeaderContainer = styled.div`
+  width: 100vw;
+  height: 30vh;
+  display: flex;
+  justify-content: center;
+  align-items: end;
+`;
+
+const HederText = styled.div`
+  color: #b9f82b;
+  font-size: 50px;
+  font-family: "En";
+  margin-bottom: 20px;
+`;
+
+const LoginContainer = styled.div`
+  width: 100vw;
+  height: 36vh;
+  background-color: #212529;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginBox = styled.div`
+  width: 23vw;
+  height: 36vh;
+  background-color: #ededed;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginInnerBox = styled.div`
+  width: 20vw;
+  height: 30vh;
+`;
+
+const LoginInputBox = styled.div`
+  width: 20vw;
+  height: 23vh;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: start;
+  flex-direction: column;
+`;
+
+const LoginInputText = styled.div`
+  width: 20vw;
+  height: 3vh;
+  font-size: 20px;
+  color: black;
+  font-family: "Ko-Bold";
+`;
+
+const LoginInput = styled.input`
+  width: 20vw;
+  height: 5vh;
+  background-color: #e2e5e2;
+  border-radius: 5px;
+  color: grey;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  border: none;
+`;
+
+const LoginButton = styled.button`
+  width: 20vw;
+  height: 6vh;
+  border-radius: 10px;
+  color: black;
+  font-size: 20px;
+  font-family: "Ko-Bold";
+  background-color: #b9f82b;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+`;
+
+const PwInitContainer = styled.div`
+  width: 100vw;
+  height: 34vh;
+  background-color: #212529;
+  display: flex;
+  justify-content: center;
+  align-items: end;
+`;
+
+const PwInitButton = styled.button`
+  width: 18vw;
+  height: 6vh;
+  background-color: #f7482a;
+  border-radius: 10px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  font-size: 20px;
+`;
+
+function LoginPage() {
+  return (
+    <Container>
+      <HeaderContainer>
+        <HederText>BEEP!</HederText>
+      </HeaderContainer>
+      <LoginContainer>
+        <LoginBox>
+          <LoginInnerBox>
+            <LoginInputBox>
+              <LoginInputText>ID</LoginInputText>
+              <LoginInput type="text" placeholder="아이디"></LoginInput>
+              <LoginInputText>PW</LoginInputText>
+              <LoginInput type="text" placeholder="비밀번호"></LoginInput>
+            </LoginInputBox>
+            <LoginButton>로그인</LoginButton>
+          </LoginInnerBox>
+        </LoginBox>
+      </LoginContainer>
+      <PwInitContainer>
+        <PwInitButton>비밀번호 초기화</PwInitButton>
+      </PwInitContainer>
+    </Container>
+  );
+}
+
+export default LoginPage;
