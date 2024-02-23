6a0bccfe95483f17fdba58bc5eb72c31ad149df1
BiteSnail <rkwoal216@gmail.com>
2024-02-23 14:04:31 +0900

    fix: cors 설정 추가

--- beep/src/main/java/com/cosmic/beep/configs/WebMvcConfig.java
+++ beep/src/main/java/com/cosmic/beep/configs/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.cosmic.beep.configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,5 +12,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
+    }
+
+    //CORS CONFIG
+    //배포 전 수정 필수
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
     }
 }
