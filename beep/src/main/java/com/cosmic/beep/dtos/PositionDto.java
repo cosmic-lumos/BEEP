896a3d383a1a66fb2c174947662b87de5e0987a0
BiteSnail <rkwoal216@gmail.com>
2024-02-23 14:07:28 +0900

    feat: position id도 포함해서 전달

--- beep/src/main/java/com/cosmic/beep/dtos/PositionDto.java
+++ beep/src/main/java/com/cosmic/beep/dtos/PositionDto.java
@@ -7,9 +7,10 @@ import lombok.Data;
 import java.util.List;
 
 @Builder
-public record PositionDto(String name, List<GoodsDto> goods) {
+public record PositionDto(Long id, String name, List<GoodsDto> goods) {
     public static PositionDto of(Positions positions){
         return PositionDto.builder()
+                .id(positions.getId())
                 .name(positions.getName())
                 .goods(positions.getGoods().stream().map(GoodsDto::of).toList())
                 .build();
