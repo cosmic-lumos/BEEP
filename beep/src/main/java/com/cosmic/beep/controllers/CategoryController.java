58a95d6312a83335c1e7be24e37a95948696cb93
BiteSnail <rkwoal216@gmail.com>
2024-02-23 14:07:37 +0900

    docs: api tag 추가

--- beep/src/main/java/com/cosmic/beep/controllers/CategoryController.java
+++ beep/src/main/java/com/cosmic/beep/controllers/CategoryController.java
@@ -3,12 +3,14 @@ package com.cosmic.beep.controllers;
 import com.cosmic.beep.dtos.CategoryDto;
 import com.cosmic.beep.entities.Category;
 import com.cosmic.beep.repositories.CategoryRepository;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@Tag(name="위치 및 카테고리 관련", description = "위치 및 카테고리를 관리하는 API입니다.")
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
