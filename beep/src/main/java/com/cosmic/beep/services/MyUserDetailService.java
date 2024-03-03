569c59bcc9b4c409a7ffb6cedf446a817fcb96cd
BiteSnail <rkwoal216@gmail.com>
2024-03-03 12:28:26 +0900

    feat: security 추가

--- beep/src/main/java/com/cosmic/beep/services/MyUserDetailService.java
+++ beep/src/main/java/com/cosmic/beep/services/MyUserDetailService.java
@@ -24,6 +24,6 @@ public class MyUserDetailService implements UserDetailsService {
         Set<GrantedAuthority> authorities = user.getRoles().stream()
                 .map((role) -> new SimpleGrantedAuthority(role.getName()))
                 .collect(Collectors.toSet());
-        return new org.springframework.security.core.userdetails.User(username,user.getPassword(),authorities);
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(),authorities);
     }
 }
