3408600859565e7ac91d216a50184a385632fae6
BiteSnail <rkwoal216@gmail.com>
2024-03-03 14:27:21 +0900

    feat: 회원가입 중 발생가능한 에러 대처

--- beep/src/main/java/com/cosmic/beep/controllers/AuthController.java
+++ beep/src/main/java/com/cosmic/beep/controllers/AuthController.java
@@ -4,6 +4,8 @@ import com.cosmic.beep.dtos.UserCreateDto;
 import com.cosmic.beep.dtos.UserDto;
 import com.cosmic.beep.dtos.accounts.LoginDto;
 import com.cosmic.beep.entities.User;
+import com.cosmic.beep.exceptions.ResourceNotFound;
+import com.cosmic.beep.exceptions.SignUpException;
 import com.cosmic.beep.repositories.RoleRepository;
 import com.cosmic.beep.repositories.UserRepository;
 import io.swagger.v3.oas.annotations.Operation;
@@ -18,6 +20,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -49,13 +54,17 @@ public class AuthController {
     )
     public UserDto createUser(@RequestBody @Valid UserCreateDto userCreateDto){
         if(!userCreateDto.password().equals(userCreateDto.matchingPassword())){
-            throw new RuntimeException("비밀번호가 일치하지 않습니다.");
+            throw new SignUpException("비밀번호가 일치하지 않습니다.");
+        }
+        if(userRepository.findByUsernameOrEmail(userCreateDto.username(), userCreateDto.email()).isPresent()){
+            throw new SignUpException("이미 존재하는 계정입니다.");
         }
         return UserDto.of(userRepository.save(User.builder()
                 .username(userCreateDto.username())
                 .password(passwordEncoder.encode(userCreateDto.password()))
                 .firstName(userCreateDto.firstName())
                 .lastName(userCreateDto.lastName())
+                .roles(Collections.singletonList(roleRepository.findByName("ROLE_USER").orElseThrow(() -> new ResourceNotFound(0L))))
                 .email(userCreateDto.email())
                 .build()));
     }
---
+++ beep/src/main/java/com/cosmic/beep/exceptions/SignUpException.java
@@ -0,0 +1,5 @@
+package com.cosmic.beep.exceptions;
+
+public class SignUpException extends RuntimeException{
+    public SignUpException(String message){ super(message);}
+}
